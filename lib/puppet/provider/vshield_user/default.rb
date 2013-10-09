d5082f26d1c03bfe75bef19f77eb669b106caabf
grosas <grosas@vmware.com>
2013-10-09 00:40:54 -0500

    vshield user provider draft

--- lib/puppet/provider/vshield_user/default.rb
+++ lib/puppet/provider/vshield_user/default.rb
@@ -2,17 +2,33 @@
 provider_path = Pathname.new(__FILE__).parent.parent
 require File.join(provider_path, 'vshield')
 
-Puppet::Type.type(:vshield_syslog).provide(:vs_syslog, :parent => Puppet::Provider::Vshield) do
-  @doc = 'Manages vShield hosts syslog configuration.'
+# Role Management 
+# http://www.vmware.com/pdf/vshield_51_api.pdf Pg. 28
+# Adding a user is not dc
 
-  def server_info
-    result = get('api/2.0/services/syslog/config')
-    nested_value(result, ['syslogServerConfig', 'serverInfo'], 'not defined.')
+Puppet::Type.type(:vshield_user).provide(:default, :parent => Puppet::Provider::Vshield) do
+  @doc = 'Manages vShield user configuration.'
+
+  def userExists?
+    results = ensure_array( nested_value(get("/api/2.0/services/usermgmt/users/vsm"), ['user', 'userInfo']) )
+    # If there's a single application the result is a hash, while multiple results in an array.
+    @userInfo = results.find {|userInfo| userInfo['name'] == resource[:name]}
   end
 
-  def server_info=(value)
-    setting = { 'syslogServerConfig' =>
-                { 'serverInfo' => value } }
-    put('api/2.0/services/syslog/config', setting)
+  def addUser
+    data = {
+      :name               		=> resource[:name],
+      :isEnabled 		  		=> true,
+      :isGroup			  		=> false,
+      :hasGlobalObjectAccess	=> true,
+      :accessControlEntry		=> { :role => resource[:role],
+      }
+    }
+    post("/api/2.0/services/usermgmt/user/local", {:userInfo => data} )
   end
+
+  def role
+    @userInfo['accessControlEntry']['role']
+  end
+
 end
